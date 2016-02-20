76d26359c6e4ce0e6625fb5f3280612bb21f1aa8
tschmidt64 <tschmidt64@utexas.edu>
2016-02-20 13:57:49 -0600

    Just added some error code, I'm just checking a git thing

--- Shuttle/ViewController.swift
+++ Shuttle/ViewController.swift
@@ -14,12 +14,12 @@ import ArcGIS
 class ViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager: CLLocationManager!
     let regionRadius: CLLocationDistance = 1000
-    
+
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         let initialLocation = CLLocation(latitude: 30.302135, longitude: -97.740153)
         centerMapOnLocation(initialLocation)
         locationManager = CLLocationManager()
@@ -28,18 +28,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         getData()
         var timer = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: "getData", userInfo: nil, repeats: true)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     func getData() {
         let urlPath = "https://data.texas.gov/download/cuc7-ywmd/text/plain"
         let url:NSURL? = NSURL(string: urlPath)
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithURL(url!) { (data, response, error) -> Void in
             if error != nil {
+                print("error found")
             } else {
                 do {
                     let jsonResult = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
@@ -53,13 +54,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                                 let route:String = tripSub["route_id"] as! String
                                 let lat:Double = positionSub["latitude"] as! Double
                                 let long:Double = positionSub ["longitude"] as! Double
-                                
+
                                 // Create annotation from lattitude and longitude
                                 let coord: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: lat, longitude: long)
                                 let annotation = MapPin(coordinate: coord, title: "Route" + route, subtitle: "")
                                 self.mapView.addAnnotation(annotation)
                                 self.centerMapOnLocation(CLLocation(latitude: lat, longitude: long))
-                                
+
                                 print("Route ID: " + route)
                                 print("Lat: " + String(lat))
                                 print("Long:" + String(long))
@@ -70,13 +71,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                         }
                     }
                 } catch {
-                    
+                    print("Exception found")
                 }
             }
         }
         task.resume() // start the request */
     }
-    
+
     // Called by mapview when adding new annotation
     func mapView(mapView: MKMapView, annotation: MapPin) -> MKPinAnnotationView {
         // Remove all annotations from the map view
@@ -85,13 +86,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         view.canShowCallout = true
         return view
     }
-    
+
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
             regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
     }
-    
+
     func checkLocationAuthorizationStatus() {
         if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
             mapView.showsUserLocation = true
@@ -99,14 +100,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.requestWhenInUseAuthorization()
         }
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         checkLocationAuthorizationStatus()
     }
 
-    
-    
-    
+
+
+
 }
 
