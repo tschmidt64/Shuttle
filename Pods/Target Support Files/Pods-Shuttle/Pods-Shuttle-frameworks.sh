833aecfdf3c09f777ab2c263a30dbdc99ab40bcd
tschmidt64 <tschmidt64@utexas.edu>
2016-03-31 18:23:00 -0500

    Renamed code to Shuttle again. Should build easily
    Don't forget to 'pod install' before running

--- Pods/Target Support Files/Pods-Shuttle/Pods-Shuttle-frameworks.sh
+++ Pods/Target Support Files/Pods-Shuttle/Pods-Shuttle-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-Clutch/SwiftyJSON.framework"
+  install_framework "Pods-Shuttle/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-Clutch/SwiftyJSON.framework"
+  install_framework "Pods-Shuttle/SwiftyJSON.framework"
 fi
---
+++ Pods/Target Support Files/Pods-Shuttle/Pods-Shuttle-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ShuttleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ShuttleVersionString[];
+
