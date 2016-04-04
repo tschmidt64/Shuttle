d3b7012ce04789be213d3e1e4fcd9aadf5049810
tschmidt64 <tschmidt64@utexas.edu>
2016-04-04 04:24:42 -0500

    Made casing of words in stop names nicer

--- Shuttle/Stop.swift
+++ Shuttle/Stop.swift
@@ -16,7 +16,35 @@ class Stop {
     
     init(location loc: CLLocationCoordinate2D, name: String, stopID id: String) {
         self.location = loc
-        self.name = name
+        self.name = name.lowercaseString.toUpperCaseFirstLetters
         self.stopId = id
+    }
+}
+
+extension String {
+    var toUpperCaseFirstLetters: String {
+        get {
+            var newCharArr: [String] = []
+            let wordsArr = self.componentsSeparatedByString(" ")
+            for word in wordsArr {
+                var wordCharArr = word.characters.map() { String($0) }
+                // If empty this will crash
+                if !wordCharArr.isEmpty {
+                    wordCharArr[wordCharArr.startIndex] = wordCharArr.first!.uppercaseString
+                    newCharArr += wordCharArr
+                    newCharArr.append(" ")
+                }
+            }
+            let newStr = newCharArr.joinWithSeparator("")
+            // if empty string this will crash
+            if !newStr.isEmpty {
+                // truncate to get rid of
+                let truncated = newStr.substringToIndex(newStr.endIndex.predecessor())
+                return truncated
+            } else {
+                return ""
+            }
+            
+        }
     }
 }
